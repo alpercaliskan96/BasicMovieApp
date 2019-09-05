ed4883d589e0f563005b2587e232c8409fef42c6
Alper Çalışkan <alpercaliskan96@gmail.com>
2019-09-06 02:59:52 +0300

    FirstCommitA8

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'movies', //<movies></movies> --> #movies = <div id="movies"></div>
+    template : '<h2>Movies</h2>'
+
+})
+export class MoviesComponent{
+
+}
