3bb2948f0eb9ee9bad73dd3821199aece112244d
Alper Çalışkan <alpercaliskan96@gmail.com>
2019-09-06 03:30:29 +0300

    FirstA8App
    Basic Movie App

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector : 'movies', //<movies></movies> --> #movies = <div id="movies"></div>
-    template : '<h2>Movies</h2>'
+    templateUrl : 'movies.component.html'
 
 })
 export class MoviesComponent{
