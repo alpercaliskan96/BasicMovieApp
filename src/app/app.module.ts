cf9f87d0849739bbbbc306e1a88d3bb1b2ce3d92
Alper Çalışkan <alpercaliskan96@gmail.com>
2019-09-06 03:31:58 +0300

    Revert "FirstA8App"
    This reverts commit 3bb2948f0eb9ee9bad73dd3821199aece112244d.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
-import { MovieComponent } from './movie/movie.component';
  
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent,
-    MovieComponent
+    MoviesComponent
   ],
   imports: [
     BrowserModule
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector : 'movies', //<movies></movies> --> #movies = <div id="movies"></div>
-    templateUrl : 'movies.component.html'
+    template : '<h2>Movies</h2>'
 
 })
 export class MoviesComponent{
